d1bb38fc228d8af34b24fb0a832e3226b039925e
Pasindu Perera <pasindup@leapset.com>
2015-03-01 18:37:44 +0530

    ui bootstrap added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,26 @@
 angular.module('vspApp')
   .controller('MainCtrl', function ($scope, flowplayer) {
     var player = flowplayer('player');
+    $scope.alerts = [
+      //{ type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+      //{ type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+
+    $scope.addAlert = function() {
+      $scope.alerts.push({msg: 'Another alert!'});
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+
+    $scope.updateVideo = function(){
+      console.log($scope.video);
+    };
+
+    $scope.updateSubtitles = function(){
+      console.log($scope.subtitle);
+    };
+
+
   });
